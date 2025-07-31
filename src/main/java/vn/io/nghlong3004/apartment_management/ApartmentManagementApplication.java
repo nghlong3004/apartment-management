a58237174e903a9c5655abf446a621731c0bc723
nghlong3004 <nghlong3004@gmail.com>
2025-07-31 20:25:03 +0700

    Initial Project

---
+++ src/main/java/vn/io/nghlong3004/apartment_management/ApartmentManagementApplication.java
@@ -0,0 +1,13 @@
+package vn.io.nghlong3004.apartment_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/io/nghlong3004/apartment_management/ApartmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.io.nghlong3004.apartment_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
