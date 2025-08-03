ba2cd04faf912bfa49d02498f2b26b7499a72ca0
nghlong3004 <nghlong3004@gmail.com>
2025-08-03 14:46:40 +0700

    feat: change name MessageUtil to MessageConstants and English instead of

--- src/main/java/vn/io/nghlong3004/apartment_management/util/MessageConstants.java
+++ src/main/java/vn/io/nghlong3004/apartment_management/util/MessageConstants.java
@@ -1,7 +1,7 @@
 package vn.io.nghlong3004.apartment_management.util;
 
-public class MessageUtil {
-	public static final String UNWANTED_EXCEPTION = "Có lỗi không mong muốn xảy ra, vui lòng thử lại sau";
+public class MessageConstants {
+	public static final String UNWANTED_EXCEPTION = "An unexpected error occurred, please try again later.";
 	public static final String EXISTS_EMAIL = "Email may already be in use.";
 	public static final String DATABASE_EXCEPTION = "Data conflicts or constraint violations.";
 }
