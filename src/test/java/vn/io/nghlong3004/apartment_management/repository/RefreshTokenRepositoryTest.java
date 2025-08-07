aad72cc582612b9414dc2fd6ecd7e0acb202bb6d
nghlong3004 <nghlong3004@gmail.com>
2025-08-07 17:35:50 +0700

    feat: Add test package repository

---
+++ src/test/java/vn/io/nghlong3004/apartment_management/repository/RefreshTokenRepositoryTest.java
@@ -0,0 +1,93 @@
+package vn.io.nghlong3004.apartment_management.repository;
+
+import static org.assertj.core.api.Assertions.within;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
+import java.util.UUID;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+
+import vn.io.nghlong3004.apartment_management.constants.ApplicationConstants;
+import vn.io.nghlong3004.apartment_management.model.RefreshToken;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class RefreshTokenRepositoryTest {
+
+	@Autowired
+	private RefreshTokenRepository refreshTokenRepository;
+
+	private final int maxTestCaseAll = 10;
+
+	private RefreshToken createRefreshToken() {
+
+		Random random = new Random();
+
+		Long userId = random.nextLong();
+
+		String token = UUID.randomUUID().toString();
+
+		RefreshToken refreshToken = RefreshToken.builder().userId(userId).token(token)
+				.expiryDate(
+						Instant.now(Clock.systemUTC()).plusMillis(ApplicationConstants.EXPIRY_DATE_REFRESH_TOKEN_MS))
+				.build();
+
+		return refreshToken;
+	}
+
+	@Test
+	@DisplayName("Method: FindByToken -> Null")
+	void RefreshTokenRepository_FindByToken_WhenTokenDoesNotExistShould_ReturnNull() {
+		for (int i = 0; i < maxTestCaseAll; ++i) {
+			String token = UUID.randomUUID().toString();
+			RefreshToken refreshToken = refreshTokenRepository.findByToken(token).orElse(null);
+			Assertions.assertThat(refreshToken).isNull();
+		}
+	}
+
+	@Test
+	@DisplayName("Method: FindByToken -> RefreshToken")
+	void RefreshTokenRepository_FindByToken_WhenTokenDoesNotExistShould_ReturnRefreshToken() {
+		for (int i = 0; i < maxTestCaseAll; ++i) {
+			RefreshToken oldRefreshToken = createRefreshToken();
+			refreshTokenRepository.save(oldRefreshToken.getUserId(), oldRefreshToken.getToken(),
+					oldRefreshToken.getExpiryDate());
+			RefreshToken refreshToken = refreshTokenRepository.findByToken(oldRefreshToken.getToken()).orElse(null);
+
+			Assertions.assertThat(refreshToken.getExpiryDate()).isCloseTo(oldRefreshToken.getExpiryDate(),
+					within(100, ChronoUnit.MILLIS));
+			refreshToken.setExpiryDate(null);
+			oldRefreshToken.setExpiryDate(null);
+			refreshToken.setId(null);
+			Assertions.assertThat(refreshToken).isEqualTo(oldRefreshToken);
+		}
+	}
+
+	@Test
+	@DisplayName("Method: DeleteByUserId")
+	void RefreshTokenRepository_DeleteByUserId_ShouldDeleteRefreshTokenByUserId() {
+		for (int i = 0; i < maxTestCaseAll; ++i) {
+			RefreshToken oldRefreshToken = createRefreshToken();
+			refreshTokenRepository.save(oldRefreshToken.getUserId(), oldRefreshToken.getToken(),
+					oldRefreshToken.getExpiryDate());
+			RefreshToken refreshToken = refreshTokenRepository.findByToken(oldRefreshToken.getToken()).orElse(null);
+			Assertions.assertThat(refreshToken).isNotNull();
+
+			refreshTokenRepository.deleteByUserId(oldRefreshToken.getUserId());
+			RefreshToken deletedRefreshToken = refreshTokenRepository.findByToken(oldRefreshToken.getToken())
+					.orElse(null);
+			Assertions.assertThat(deletedRefreshToken).isNull();
+
+		}
+	}
+
+}
--- src/test/java/vn/io/nghlong3004/apartment_management/repository/UserRepositoryTest.java
+++ src/test/java/vn/io/nghlong3004/apartment_management/repository/UserRepositoryTest.java
@@ -1,6 +1,8 @@
 package vn.io.nghlong3004.apartment_management.repository;
 
+import java.time.Instant;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
+import vn.io.nghlong3004.apartment_management.constants.ApplicationConstants;
 import vn.io.nghlong3004.apartment_management.model.Role;
 import vn.io.nghlong3004.apartment_management.model.User;
 import vn.io.nghlong3004.apartment_management.model.UserStatus;
@@ -21,8 +24,10 @@ class UserRepositoryTest {
 	@Autowired
 	private UserRepository userRepository;
 
-	private User createSampleUser() {
-		User user = User.builder().firstName("Long").lastName("Nguyen").email("nghlong3004@example.com")
+	private final int maxTestCaseAll = 10;
+
+	private User createSampleUser(String username) {
+		User user = User.builder().firstName("Long").lastName("Nguyen").email(username + "@example.com")
 				.password("matkhaune!A@1234").phoneNumber("0987654321").role(Role.USER).status(UserStatus.ACTIVE)
 				.floor(null).build();
 		return user;
@@ -32,17 +37,58 @@ class UserRepositoryTest {
 	@DisplayName("Method: ExitstByEmail -> False")
 	void UserRepository_ExistsByEmail_WhenEmailDoesNotExistShould_ReturnFalse() {
 
-		Optional<Boolean> exists = userRepository.existsByEmail("nghlong3004@example.com");
-		Assertions.assertThat(exists.orElse(false)).isEqualTo(false);
+		for (int i = 0; i < maxTestCaseAll; ++i) {
+			String username = UUID.randomUUID().toString();
+			Optional<Boolean> exists = userRepository.existsByEmail(username + "@example.com");
+			Assertions.assertThat(exists.orElse(false)).isEqualTo(false);
+		}
 	}
 
 	@Test
 	@DisplayName("Method: ExitstByEmail -> True")
 	void UserRepository_ExistsByEmail_WhenEmailExistsShould_ReturnTrue() {
-		userRepository.save(createSampleUser());
+		System.out.println(Instant.now().getEpochSecond());
+		System.out
+				.println(Instant.now().plusMillis(ApplicationConstants.EXPIRY_DATE_REFRESH_TOKEN_MS).getEpochSecond());
+		for (int i = 0; i < maxTestCaseAll; ++i) {
+			String username = UUID.randomUUID().toString();
 
-		Optional<Boolean> exists = userRepository.existsByEmail("nghlong3004@example.com");
+			userRepository.save(createSampleUser(username));
 
-		Assertions.assertThat(exists.orElse(false)).isEqualTo(true);
+			Optional<Boolean> exists = userRepository.existsByEmail(username + "@example.com");
+
+			Assertions.assertThat(exists.orElse(false)).isEqualTo(true);
+		}
 	}
+
+	@Test
+	@DisplayName("Method: FindByEmail -> User")
+	void UserRepository_FindByEmail_WhenEmailExistsShould_ReturnUserByEmail() {
+		for (int i = 0; i < maxTestCaseAll; ++i) {
+			String username = UUID.randomUUID().toString();
+
+			User user = createSampleUser(username);
+			if (!userRepository.existsByEmail(user.getEmail()).orElse(false)) {
+				userRepository.save(user);
+			}
+
+			Optional<User> userByEmail = userRepository.findByEmail(user.getEmail());
+			userByEmail.orElse(user).setId(null);
+			userByEmail.orElse(user).setCreated(null);
+			userByEmail.orElse(user).setUpdated(null);
+			Assertions.assertThat(userByEmail.orElse(null)).isEqualTo(user);
+		}
+	}
+
+	@Test
+	@DisplayName("Method: FindByEmail -> Null")
+	void UserRepository_FindByEmail_WhenEmailExistsShould_ReturnNull() {
+		for (int i = 0; i < maxTestCaseAll; ++i) {
+			String username = UUID.randomUUID().toString();
+
+			Optional<User> userByEmail = userRepository.findByEmail(username + "@example.com");
+			Assertions.assertThat(userByEmail.orElse(null)).isNull();
+		}
+	}
+
 }
