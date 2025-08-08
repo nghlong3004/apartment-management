b3cae8c2fd1664eb32ffb53f630dc9c50d297913
nghlong3004 <nghlong3004@gmail.com>
2025-08-09 00:21:17 +0700

    feat: fix ErrorState

--- src/main/java/vn/io/nghlong3004/apartment_management/exception/ErrorState.java
+++ src/main/java/vn/io/nghlong3004/apartment_management/exception/ErrorState.java
@@ -9,8 +9,8 @@ import lombok.Getter;
 @Getter
 public enum ErrorState {
 	UNWANTED_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred, please try again later."),
-	EXISTS_EMAIL(HttpStatus.BAD_GATEWAY, "Email may already be in use."),
-	REFRESH_TOKEN_EXPIRED(HttpStatus.BAD_GATEWAY, "Refresh token was expired. Please make a new signin request."),
+	EXISTS_EMAIL(HttpStatus.BAD_REQUEST, "Email may already be in use."),
+	REFRESH_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "Refresh token was expired. Please make a new signin request."),
 	DATABASE_EXCEPTION(HttpStatus.CONFLICT, "Data conflicts or constraint violations."),
 	LOGIN_FALSE(HttpStatus.BAD_REQUEST, "Incorrect email or password."),
 	ERROR_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "Refresh Token expired or not available."),
