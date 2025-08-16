87df82c2495feff64eee5869d93f617ab20dbeb4
nghlong3004 <nghlong3004@gmail.com>
2025-08-16 18:58:24 +0700

    feat: add config mybatis auto map underscore to camelcase in application.properties and fix server return null field

--- src/main/java/vn/io/nghlong3004/apartment_management/config/MyBatisConfig.java
+++ src/main/java/vn/io/nghlong3004/apartment_management/config/MyBatisConfig.java
@@ -1,16 +1,5 @@
 package vn.io.nghlong3004.apartment_management.config;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("vn.io.nghlong3004.apartment_management.repository")
 public class MyBatisConfig {
-	@Bean
-	public org.apache.ibatis.session.Configuration myBatisConfiguration() {
-		org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
-		configuration.setMapUnderscoreToCamelCase(true);
-		return configuration;
-	}
+
 }
